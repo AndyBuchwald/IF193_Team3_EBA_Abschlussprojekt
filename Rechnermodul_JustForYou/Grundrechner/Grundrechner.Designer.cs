a691ebcc72d4f10b1c96c617576dc04c8155e04c
Donath1012 <78794796+Donath1012@users.noreply.github.com>
2021-06-07 12:10:21 +0200

    Add files via upload
    Added first Project Version

---
+++ Rechnermodul_JustForYou/Grundrechner/Grundrechner.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace Rechnermodul_JustForYou.Grundrechner
+{
+    partial class Grundrechner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGrdSet = new System.Windows.Forms.Button();
+            this.btnGrdCls = new System.Windows.Forms.Button();
+            this.GrdTxt = new System.Windows.Forms.RichTextBox();
+            this.button24 = new System.Windows.Forms.Button();
+            this.btnGrdMin = new System.Windows.Forms.Button();
+            this.btnGrdPlus = new System.Windows.Forms.Button();
+            this.btnGrdDiv = new System.Windows.Forms.Button();
+            this.btnGrdMlt = new System.Windows.Forms.Button();
+            this.btnGrdRoot = new System.Windows.Forms.Button();
+            this.btnGrdSqr = new System.Windows.Forms.Button();
+            this.btnGrdBrRght = new System.Windows.Forms.Button();
+            this.btnGrdBrLft = new System.Windows.Forms.Button();
+            this.btnGrdSgn = new System.Windows.Forms.Button();
+            this.btnGrdThr = new System.Windows.Forms.Button();
+            this.btnGrdSix = new System.Windows.Forms.Button();
+            this.btnGrdNine = new System.Windows.Forms.Button();
+            this.btnGrdBck = new System.Windows.Forms.Button();
+            this.btnGrdCmm = new System.Windows.Forms.Button();
+            this.btnGrdTwo = new System.Windows.Forms.Button();
+            this.btnGrdFive = new System.Windows.Forms.Button();
+            this.btnGrdEight = new System.Windows.Forms.Button();
+            this.btnGrdClr = new System.Windows.Forms.Button();
+            this.btnGrdZero = new System.Windows.Forms.Button();
+            this.btnGrdOne = new System.Windows.Forms.Button();
+            this.btnGrdFour = new System.Windows.Forms.Button();
+            this.btnGrdSvn = new System.Windows.Forms.Button();
+            this.btnGrdCE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGrdSet
+            // 
+            this.btnGrdSet.Location = new System.Drawing.Point(284, 11);
+            this.btnGrdSet.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdSet.Name = "btnGrdSet";
+            this.btnGrdSet.Size = new System.Drawing.Size(34, 38);
+            this.btnGrdSet.TabIndex = 53;
+            this.btnGrdSet.Text = "⚙";
+            this.btnGrdSet.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdCls
+            // 
+            this.btnGrdCls.Location = new System.Drawing.Point(323, 11);
+            this.btnGrdCls.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdCls.Name = "btnGrdCls";
+            this.btnGrdCls.Size = new System.Drawing.Size(34, 38);
+            this.btnGrdCls.TabIndex = 52;
+            this.btnGrdCls.Text = "X";
+            this.btnGrdCls.UseVisualStyleBackColor = true;
+            // 
+            // GrdTxt
+            // 
+            this.GrdTxt.Location = new System.Drawing.Point(11, 61);
+            this.GrdTxt.Margin = new System.Windows.Forms.Padding(2);
+            this.GrdTxt.Name = "GrdTxt";
+            this.GrdTxt.Size = new System.Drawing.Size(349, 79);
+            this.GrdTxt.TabIndex = 51;
+            this.GrdTxt.Text = "";
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(223, 386);
+            this.button24.Margin = new System.Windows.Forms.Padding(2);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(136, 51);
+            this.button24.TabIndex = 50;
+            this.button24.Text = "=";
+            this.button24.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdMin
+            // 
+            this.btnGrdMin.Location = new System.Drawing.Point(293, 330);
+            this.btnGrdMin.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdMin.Name = "btnGrdMin";
+            this.btnGrdMin.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdMin.TabIndex = 49;
+            this.btnGrdMin.Text = "-";
+            this.btnGrdMin.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdPlus
+            // 
+            this.btnGrdPlus.Location = new System.Drawing.Point(223, 330);
+            this.btnGrdPlus.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdPlus.Name = "btnGrdPlus";
+            this.btnGrdPlus.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdPlus.TabIndex = 48;
+            this.btnGrdPlus.Text = "+";
+            this.btnGrdPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdDiv
+            // 
+            this.btnGrdDiv.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnGrdDiv.Location = new System.Drawing.Point(293, 274);
+            this.btnGrdDiv.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdDiv.Name = "btnGrdDiv";
+            this.btnGrdDiv.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdDiv.TabIndex = 47;
+            this.btnGrdDiv.Text = "/";
+            this.btnGrdDiv.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdMlt
+            // 
+            this.btnGrdMlt.Location = new System.Drawing.Point(223, 274);
+            this.btnGrdMlt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdMlt.Name = "btnGrdMlt";
+            this.btnGrdMlt.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdMlt.TabIndex = 46;
+            this.btnGrdMlt.Text = "x";
+            this.btnGrdMlt.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdRoot
+            // 
+            this.btnGrdRoot.Location = new System.Drawing.Point(293, 218);
+            this.btnGrdRoot.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdRoot.Name = "btnGrdRoot";
+            this.btnGrdRoot.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdRoot.TabIndex = 45;
+            this.btnGrdRoot.Text = "√";
+            this.btnGrdRoot.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdSqr
+            // 
+            this.btnGrdSqr.Location = new System.Drawing.Point(223, 218);
+            this.btnGrdSqr.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdSqr.Name = "btnGrdSqr";
+            this.btnGrdSqr.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdSqr.TabIndex = 44;
+            this.btnGrdSqr.Text = "x²";
+            this.btnGrdSqr.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdBrRght
+            // 
+            this.btnGrdBrRght.Location = new System.Drawing.Point(293, 162);
+            this.btnGrdBrRght.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdBrRght.Name = "btnGrdBrRght";
+            this.btnGrdBrRght.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdBrRght.TabIndex = 43;
+            this.btnGrdBrRght.Text = ")";
+            this.btnGrdBrRght.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdBrLft
+            // 
+            this.btnGrdBrLft.Location = new System.Drawing.Point(223, 162);
+            this.btnGrdBrLft.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdBrLft.Name = "btnGrdBrLft";
+            this.btnGrdBrLft.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdBrLft.TabIndex = 42;
+            this.btnGrdBrLft.Text = "(";
+            this.btnGrdBrLft.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdSgn
+            // 
+            this.btnGrdSgn.Location = new System.Drawing.Point(152, 386);
+            this.btnGrdSgn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdSgn.Name = "btnGrdSgn";
+            this.btnGrdSgn.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdSgn.TabIndex = 41;
+            this.btnGrdSgn.Text = "+ / -";
+            this.btnGrdSgn.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdThr
+            // 
+            this.btnGrdThr.Location = new System.Drawing.Point(152, 330);
+            this.btnGrdThr.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdThr.Name = "btnGrdThr";
+            this.btnGrdThr.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdThr.TabIndex = 40;
+            this.btnGrdThr.Text = "3";
+            this.btnGrdThr.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdSix
+            // 
+            this.btnGrdSix.Location = new System.Drawing.Point(152, 274);
+            this.btnGrdSix.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdSix.Name = "btnGrdSix";
+            this.btnGrdSix.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdSix.TabIndex = 39;
+            this.btnGrdSix.Text = "6";
+            this.btnGrdSix.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdNine
+            // 
+            this.btnGrdNine.Location = new System.Drawing.Point(152, 218);
+            this.btnGrdNine.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdNine.Name = "btnGrdNine";
+            this.btnGrdNine.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdNine.TabIndex = 38;
+            this.btnGrdNine.Text = "9";
+            this.btnGrdNine.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdBck
+            // 
+            this.btnGrdBck.Location = new System.Drawing.Point(152, 162);
+            this.btnGrdBck.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdBck.Name = "btnGrdBck";
+            this.btnGrdBck.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdBck.TabIndex = 37;
+            this.btnGrdBck.Text = "←";
+            this.btnGrdBck.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdCmm
+            // 
+            this.btnGrdCmm.Location = new System.Drawing.Point(81, 386);
+            this.btnGrdCmm.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdCmm.Name = "btnGrdCmm";
+            this.btnGrdCmm.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdCmm.TabIndex = 36;
+            this.btnGrdCmm.Text = ",";
+            this.btnGrdCmm.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdTwo
+            // 
+            this.btnGrdTwo.Location = new System.Drawing.Point(81, 330);
+            this.btnGrdTwo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdTwo.Name = "btnGrdTwo";
+            this.btnGrdTwo.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdTwo.TabIndex = 35;
+            this.btnGrdTwo.Text = "2";
+            this.btnGrdTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdFive
+            // 
+            this.btnGrdFive.Location = new System.Drawing.Point(81, 274);
+            this.btnGrdFive.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdFive.Name = "btnGrdFive";
+            this.btnGrdFive.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdFive.TabIndex = 34;
+            this.btnGrdFive.Text = "5";
+            this.btnGrdFive.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdEight
+            // 
+            this.btnGrdEight.Location = new System.Drawing.Point(81, 218);
+            this.btnGrdEight.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdEight.Name = "btnGrdEight";
+            this.btnGrdEight.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdEight.TabIndex = 33;
+            this.btnGrdEight.Text = "8";
+            this.btnGrdEight.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdClr
+            // 
+            this.btnGrdClr.Location = new System.Drawing.Point(81, 162);
+            this.btnGrdClr.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdClr.Name = "btnGrdClr";
+            this.btnGrdClr.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdClr.TabIndex = 32;
+            this.btnGrdClr.Text = "C";
+            this.btnGrdClr.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdZero
+            // 
+            this.btnGrdZero.Location = new System.Drawing.Point(11, 386);
+            this.btnGrdZero.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdZero.Name = "btnGrdZero";
+            this.btnGrdZero.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdZero.TabIndex = 31;
+            this.btnGrdZero.Text = "0";
+            this.btnGrdZero.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdOne
+            // 
+            this.btnGrdOne.Location = new System.Drawing.Point(11, 330);
+            this.btnGrdOne.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdOne.Name = "btnGrdOne";
+            this.btnGrdOne.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdOne.TabIndex = 30;
+            this.btnGrdOne.Text = "1";
+            this.btnGrdOne.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdFour
+            // 
+            this.btnGrdFour.Location = new System.Drawing.Point(11, 274);
+            this.btnGrdFour.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdFour.Name = "btnGrdFour";
+            this.btnGrdFour.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdFour.TabIndex = 29;
+            this.btnGrdFour.Text = "4";
+            this.btnGrdFour.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdSvn
+            // 
+            this.btnGrdSvn.Location = new System.Drawing.Point(11, 218);
+            this.btnGrdSvn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdSvn.Name = "btnGrdSvn";
+            this.btnGrdSvn.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdSvn.TabIndex = 28;
+            this.btnGrdSvn.Text = "7";
+            this.btnGrdSvn.UseVisualStyleBackColor = true;
+            // 
+            // btnGrdCE
+            // 
+            this.btnGrdCE.Location = new System.Drawing.Point(11, 162);
+            this.btnGrdCE.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrdCE.Name = "btnGrdCE";
+            this.btnGrdCE.Size = new System.Drawing.Size(66, 51);
+            this.btnGrdCE.TabIndex = 27;
+            this.btnGrdCE.Text = "CE";
+            this.btnGrdCE.UseVisualStyleBackColor = true;
+            // 
+            // Grundrechner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 450);
+            this.Controls.Add(this.btnGrdSet);
+            this.Controls.Add(this.btnGrdCls);
+            this.Controls.Add(this.GrdTxt);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.btnGrdMin);
+            this.Controls.Add(this.btnGrdPlus);
+            this.Controls.Add(this.btnGrdDiv);
+            this.Controls.Add(this.btnGrdMlt);
+            this.Controls.Add(this.btnGrdRoot);
+            this.Controls.Add(this.btnGrdSqr);
+            this.Controls.Add(this.btnGrdBrRght);
+            this.Controls.Add(this.btnGrdBrLft);
+            this.Controls.Add(this.btnGrdSgn);
+            this.Controls.Add(this.btnGrdThr);
+            this.Controls.Add(this.btnGrdSix);
+            this.Controls.Add(this.btnGrdNine);
+            this.Controls.Add(this.btnGrdBck);
+            this.Controls.Add(this.btnGrdCmm);
+            this.Controls.Add(this.btnGrdTwo);
+            this.Controls.Add(this.btnGrdFive);
+            this.Controls.Add(this.btnGrdEight);
+            this.Controls.Add(this.btnGrdClr);
+            this.Controls.Add(this.btnGrdZero);
+            this.Controls.Add(this.btnGrdOne);
+            this.Controls.Add(this.btnGrdFour);
+            this.Controls.Add(this.btnGrdSvn);
+            this.Controls.Add(this.btnGrdCE);
+            this.Name = "Grundrechner";
+            this.Text = "Grundrechner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGrdSet;
+        private System.Windows.Forms.Button btnGrdCls;
+        private System.Windows.Forms.RichTextBox GrdTxt;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button btnGrdMin;
+        private System.Windows.Forms.Button btnGrdPlus;
+        private System.Windows.Forms.Button btnGrdDiv;
+        private System.Windows.Forms.Button btnGrdMlt;
+        private System.Windows.Forms.Button btnGrdRoot;
+        private System.Windows.Forms.Button btnGrdSqr;
+        private System.Windows.Forms.Button btnGrdBrRght;
+        private System.Windows.Forms.Button btnGrdBrLft;
+        private System.Windows.Forms.Button btnGrdSgn;
+        private System.Windows.Forms.Button btnGrdThr;
+        private System.Windows.Forms.Button btnGrdSix;
+        private System.Windows.Forms.Button btnGrdNine;
+        private System.Windows.Forms.Button btnGrdBck;
+        private System.Windows.Forms.Button btnGrdCmm;
+        private System.Windows.Forms.Button btnGrdTwo;
+        private System.Windows.Forms.Button btnGrdFive;
+        private System.Windows.Forms.Button btnGrdEight;
+        private System.Windows.Forms.Button btnGrdClr;
+        private System.Windows.Forms.Button btnGrdZero;
+        private System.Windows.Forms.Button btnGrdOne;
+        private System.Windows.Forms.Button btnGrdFour;
+        private System.Windows.Forms.Button btnGrdSvn;
+        private System.Windows.Forms.Button btnGrdCE;
+    }
+}
